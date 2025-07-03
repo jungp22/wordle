b8bd1e20347be64706a4657e567be560dd475ab6
Jung-Hwan Park <jungpark.2002@gmail.com>
2025-07-03 11:05:09 -0600

    can input letters, can also check them a bit

--- wordle.js
+++ wordle.js
@@ -60,32 +60,118 @@ function showHint() {
   }
 }
 
-function isAlpha(char) {
-  return char.key.length===1;
-}
-const game = document.querySelector("body");
-function gameLogic(event) {
-  if (isAlpha(event)) {
-    letter = event.key.toUpperCase();
+let word = [];
+document.addEventListener("keydown", keyPressed);
+
+function keyPressed(event) {
+  if (word.length < 4 && isLetter(event.key)) {
+    word.push(event.key.toUpperCase());
+
     for (let x = 0; x < 16; x++) {
       if (document.getElementsByClassName("letters")[x].innerHTML === "") {
+        console.log(x);
         document.getElementsByClassName("letters")[x].innerHTML =
-          "<p>" + letter + "</p>";
-        document.getElementsByClassName("letters")[x].class =
-          "letters attempted";
-
-        document.getElementsByClassName("letters")[x].class =
-          "letters attempted";
-
+          word[word.length - 1];
         break;
       }
     }
+    console.log(word);
+  } else if (event.key === "Backspace") {
+    word.pop();
+    console.log(word);
+    for (let x = 0; x < 16; x++) {
+      if (document.getElementsByClassName("letters")[x].innerHTML === "" && word.length >=0) {
+        if(word.length !==0){
+          document.getElementsByClassName("letters")[x-1].removeAttribute("id");}
+        document.getElementsByClassName("letters")[x-1].innerHTML ="";
+        break;
+      }
+    }
+  } else if (word.length === 4 && event.key === "Enter") {
+    let result = checkAnswer(word);
+    console.log(result);
+    for (let x = 0; x < 16; x++) {
+      if (document.getElementsByClassName("letters")[x].id !== "wrong"&&document.getElementsByClassName("letters")[x].id !== "correct" &&document.getElementsByClassName("letters")[x].id !== "misplaced") {
+        for (let y = 0; y < 4; y++) {
+          document.getElementsByClassName("letters")[x + y].id = result[y];
+        }
+        break;
+      }
+    }
+    word = [];
   }
 }
-function startOver() {
-  for (let x = 0; x < 16; x++) {
-    document.getElementsByClassName("letters")[x].innerHTML = "";
-    document.getElementsByClassName("letters")[x].removeAttribute("id");
+
+function isLetter(letter) {
+  if (letter.length === 1 && letter.match(/[a-zA-Z]/g)) {
+    return true;
+  } else {
+    return false;
   }
 }
-game.addEventListener("keydown", gameLogic);
+function checkAnswer(word) {
+  console.log(word);
+  let correctAnswer = "TEST";
+  let matches = [];
+  let res = ["wrong", "wrong", "wrong", "wrong"];
+  for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < 4; y++) {
+      if (word[x] === correctAnswer[y]) {
+        if (x === y) {
+          res[x] = "correct";
+        } else {
+          if (res[x] !== "correct") {
+            res[x] = "misplaced";
+          }
+        }
+      }
+    }
+  }
+
+  return res;
+}
+test = {
+  statusCode: 200,
+  dictionary: [
+    {
+      word: "Pain",
+      hint: "Attending any class other than ENSF 381 gives you ____",
+    },
+    {
+      word: "Nerd",
+      hint: "You may be considered one, if you like Star Trek",
+    },
+    {
+      word: "Fish",
+      hint: "A type of animal that can swim in water",
+    },
+    {
+      word: "Book",
+      hint: "Something you read to learn new things",
+    },
+    {
+      word: "Door",
+      hint: "An object that allows entry into a building",
+    },
+    {
+      word: "Rock",
+      hint: "A hard material found in nature",
+    },
+    {
+      word: "Star",
+      hint: "A celestial object in space",
+    },
+    {
+      word: "Ball",
+      hint: "An object used in sports and play",
+    },
+    {
+      word: "Sand",
+      hint: "A granular material found at beaches",
+    },
+    {
+      word: "Moon",
+      hint: "Earth's natural satellite",
+    },
+  ],
+};
